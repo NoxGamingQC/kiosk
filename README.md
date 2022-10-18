b2b0e8b23417c249f0c0ff0c8315e62e08bdb3b9
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-18 11:22:46 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # delivery-comunicator
+
 An app to communicate to the kitchen that the delivery driver is comming back to restaurant
+
+follow the setup.sh script (Not actually a script yet)
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Go to:
+#sudo nano /home/pi/config/lxsession/LXDE-pi/autostart
+# Copy autostart in the folder
+
+#copy and paste kiosk.sh in `/home/pi`
+
+#you should be good to go
