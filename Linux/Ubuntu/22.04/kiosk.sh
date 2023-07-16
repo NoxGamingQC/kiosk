96c4271083bd654f6f652257ffc87b445fdb7252
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-15 22:23:54 -0400

    Add picture size

--- Linux/Ubuntu/22.04/kiosk.sh
+++ Linux/Ubuntu/22.04/kiosk.sh
@@ -15,10 +15,10 @@ openbox --replace
 
 update-alternatives --list default.plymouth
 
-## For just the logo in the splash screen (the one in the center) change /usr/share/plymouth/themes/spinner/bgrt-fallback.png
+## For just the logo in the splash screen (the one in the center) change /usr/share/plymouth/themes/spinner/bgrt-fallback.png (128x128px)
 
-## For just the logo with the text (the one on the bottom of the screen) change /usr/share/plymouth/themes/spinner/watermark.png
+## For just the logo with the text (the one on the bottom of the screen) change /usr/share/plymouth/themes/spinner/watermark.png (248x87px)
 
-## Might need also to change /usr/share/plymouth/ubuntu-logo.png
+## Might need also to change /usr/share/plymouth/ubuntu-logo.png (248x87px)
 
 ## For all those enty above make sure to keep the same file name.
