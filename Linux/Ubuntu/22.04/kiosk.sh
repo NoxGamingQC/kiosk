e0d84e64a8bd2d3fcc27e9428e8e624517afb5fe
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-16 10:46:38 -0400

    Add color to the bash script

--- Linux/Ubuntu/22.04/kiosk.sh
+++ Linux/Ubuntu/22.04/kiosk.sh
@@ -9,12 +9,26 @@
 # LAST UPDATE: 2023-07-15
 ############################################
 
+# Variables
+INFO='\033[1;36m'
+SUCCESS='\033[1;32m'
+WARNING='\033[1;33m'
+ERROR='\033[1;31m'
+COLOR_RESET='\033[0m'
+
+# The script itself
+
+echo -e "${INFO}Installing openbox${COLOR_RESET}"
+
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox obconf
 
+echo "${INFO}Downloading Google Chrome${COLOR_RESET}"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+echo "${INFO}Installing Google Chrome${COLOR_RESET}"
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+echo "${INFO}Moving autostart${COLOR_RESET}"
+mv autostart /etc/xdg/openbox/autostart
 
-cd autostart /etc/xdg/openbox/autostart
-
-openbox --replace
+#openbox --replace
